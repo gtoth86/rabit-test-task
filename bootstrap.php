42c12c32415b91103621e20421b9eda73e2dad49
gabor <g.toth86@gmail.com>
2021-06-20 03:00:22 +0200

    require php-di package

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,10 @@
 <?php
 if (!file_exists(__DIR__ . '/app/config.php')) {
-    exit("Config file does not exist. Please create it from app/config.php.dist.");
+    exit("Config file does not exist. Please create it from app/config.php.dist." . PHP_EOL);
 }
 
 require __DIR__ . '/app/config.php';
 require __DIR__ . '/vendor/autoload.php';
+
+$builder = new DI\ContainerBuilder();
+$container = $builder->build();
