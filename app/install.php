3a5d42c40756ac5c11e9c81ffdeec2ec2aa04bea
gabor <g.toth86@gmail.com>
2021-06-20 15:12:49 +0200

    add fixtures

--- app/install.php
+++ app/install.php
@@ -28,3 +28,17 @@ $pdo->exec("CREATE TABLE advertisements (
                         FOREIGN KEY (userid) REFERENCES users(id)
                       ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
 ");
+
+require __DIR__ . "/fixtures.php";
+
+$userValues = array_map(function ($userData) {
+    return sprintf("(%d, '%s')", $userData["id"], $userData["name"]);
+}, $fixtures["users"]);
+
+$pdo->exec(sprintf("INSERT INTO users (id, name) VALUES %s", implode(", ", $userValues)));
+
+$advertisementValues = array_map(function ($advertisementData) {
+    return sprintf("(%d, %d, '%s')", $advertisementData["id"], $advertisementData["userid"], $advertisementData["title"]);
+}, $fixtures["advertisements"]);
+
+$pdo->exec(sprintf("INSERT INTO advertisements (id, userid, title) VALUES %s", implode(", ", $advertisementValues)));
